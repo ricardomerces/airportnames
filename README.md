a3663d2c1b1372d777a3c2e8f65eab36b20d3607
ricardomerces <ricardo.merces@gmail.com>
2019-05-23 18:08:04 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Fonte:
+[https://github.com/GoogleCloudPlatform/endpoints-quickstart]
+
+#Como executar
+```
+$ docker run --name airportnames -p 8080:8080 rmerces/airportnames
+```
+
+#Consulta:
+
+iataCode --> código iata dos aeroportos
+
+```
+http://localhost:8080/airportName?iataCode=SDU
+```
---
+++ app/app.py
@@ -0,0 +1,24 @@
+#Copia da main.py -->Dockerfile
+
+import logging
+
+from flask import Flask
+from flask import request
+from airports import Airports
+
+app = Flask(__name__)
+airport_util = Airports()
+
+@app.route('/airportName', methods=['GET'])
+def airportName():
+    """Given an airport IATA code, return that airport's name."""
+    iata_code = request.args.get('iataCode')
+    if iata_code is None:
+      return 'No IATA code provided.', 400
+    maybe_name = airport_util.get_airport_by_iata(iata_code)
+    if maybe_name is None:
+      return 'IATA code not found : %s' % iata_code, 400
+    return maybe_name, 200
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
