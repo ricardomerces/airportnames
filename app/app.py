22f8c7fffc342197aa2fbeee6bfad02af51cbee6
Ricardo Merces <ricardo.merces@gmail.com>
2020-01-17 05:44:26 -0300

    change 127.0.0.1 --> 0.0.0.0

--- app/app.py
+++ app/app.py
@@ -21,4 +21,4 @@ def airportName():
     return maybe_name, 200
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
